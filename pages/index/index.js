ac62b168015b90340bafa82d755ad0b596d2a8db
Lu-Zhanxing <18330259691@qq.com>
2023-07-19 23:52:21 +0800

    首页排行榜页面+动态数据展示

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,15 +7,42 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    bannerList:[],//轮播图
+    recommendList:[],//推荐歌曲
+    topList:[] //排行榜
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: async function (options) {
-    let result = await request('/banner',{type:2})
-    console.log(result);
+    // 轮播图
+    let bannerListData = await request('/banner',{type:2})
+    this.setData({
+      bannerList:bannerListData.banners
+    })
+
+    // 推荐歌曲
+    let recommendListData = await request('/personalized',{limit:10})
+    this.setData({
+      recommendList:recommendListData.result
+    })
+
+    // 排行榜
+    // 根据idx获取前5组数据，整合到数组中，供页面使用
+    let topListData = []
+    for (let index = 0; index < 5; index++) {
+      let res = await request('/top/list',{idx:index})
+      let topListItem = {
+        name:res.playlist.name,
+        tracks:res.playlist.tracks.slice(0,3)
+      }
+      topListData.push(topListItem)
+      this.setData({
+        topList:topListData
+      })
+    }
+    
   },
 
   /**
